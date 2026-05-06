b5e9eb6216558afd911a5e678a50b675a0cdb408
HCarrr <huda012354@gmail.com>
2026-05-06 07:37:08 +0700

    Menambahkan API GAme

---
+++ Modul10_103022430006/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Modul10_103022430006.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Developer { get; set; }
+        public int TahunRilis { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public List<string> Platform { get; set; }
+        public List <string> Mode { get; set; }
+        public bool IsOnline { get; set; }
+        public int Harga { get; set; }
+
+    }
+}
+
